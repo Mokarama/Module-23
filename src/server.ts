1a228518170425db5edb5837f55db23180fdeec7
Mokarama <mokaramaaktershanta@gmail.com>
2026-01-03 00:39:39 +0600

    Set up basic Express route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
+import app from "./app";
 import { prisma } from "./lib/prisma"
+
+const PORT=process.env.PORT || 3000;
 
 async function main(){
     try{
         await prisma.$connect();
+        console.log("Connect to the database successfully");
 
-
+        app.listen(PORT,()=>{
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
     }
     catch(error){
-        console.log("An error occurred: ", error)
+        console.log("An error occurred: ", error);
+
+        await prisma.$disconnect();
+        process.exit(1);
     }
 }
+
+
+main();
+
